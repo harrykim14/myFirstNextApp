46ecc14b4918bd43b0cc93bbd23a5e8b87cca7d9
harrykim14 <harrykim14@korea.ac.kr>
2021-03-16 17:27:18 +0900

    21.03.16 Changed index page for checking automatical deploy

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout title="Home">
-      <p className = "text-4xl">Welcome to Next.js!</p>
+      <p className = "text-4xl">This page made by Next.js with an online class!</p>
     </Layout>
   )
 }
